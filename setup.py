4972a1f582e26fc3cfe2c92d3ef8a7a23ea49bff
Vlad Grigorescu <grigorescu@gmail.com>
2020-04-18 20:52:26 -0500

    Move setup.py to the top level.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open('requirements.txt') as f:
+with open('brokerd/requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
@@ -12,5 +12,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    scripts=['brokerd.py'],
+    scripts=['brokerd/run_server.py'],
 )
