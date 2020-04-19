682809b7b8805944c895e3b348a7db55140fc51b
Vlad Grigorescu <grigorescu@gmail.com>
2020-04-19 15:19:50 -0500

    Fix build.sh arguments

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-$2 -m venv scripts/venv
+BIND_PORT=$2
+URL=$3
+PYTHON=$4
+
+$PYTHON -m venv scripts/venv
 source scripts/venv/bin/activate
 pip install --upgrade pip
 pip install .
@@ -11,6 +15,6 @@ chmod +x brokerd/build.py brokerd/run_brokerd.py brokerd/test.py
 
 brokerd/test.py
 
-BROKERD_PORT=$1 URL=$3 brokerd/build.py
+BROKERD_PORT=$BIND_PORT URL=$URL brokerd/build.py
 
 
