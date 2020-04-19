13041650d5a48863d8d2aeb030ded9b9dab32d61
Vlad Grigorescu <grigorescu@gmail.com>
2020-04-18 21:26:04 -0500

    Fix pip install command.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 /usr/bin/env python3 -m venv scripts/venv
 source scripts/venv/bin/activate
 pip install --upgrade pip
-pip install ./brokerd
+pip install .
 
 chmod +x brokerd/build.py brokerd/run_brokerd.py
 
