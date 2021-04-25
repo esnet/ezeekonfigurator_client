934e42fe8881370b32d6e6cd3165cbd6cf41f170
Vlad Grigorescu <vlad@es.net>
2021-04-25 10:30:36 -0500

    Convert logic to a function, so that it can be used in a test.

--- brokerd/build.py
+++ brokerd/build.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
-from jinja2 import Template
 import os
 import uuid
+
+from jinja2 import Template
 
 data = {
     'brokerd_port': os.environ.get("BROKERD_PORT", "47000"),
     'brokerd_address': os.environ.get("BROKERD_ADDRESS", "127.0.0.1"),
     'uuid': os.environ.get("UUID", uuid.uuid4()),
     'url': os.environ.get("URL", "http://localhost:8000"),
-    }
+}
 
-with open("scripts/communication.zeek.j2", 'r') as f:
-    template = Template(f.read())
 
-with open("scripts/communication.zeek", 'w') as f:
-    f.write(template.render(**data))
+def render(filename):
+    with open("scripts/communication.zeek.j2", 'r') as f:
+        template = Template(f.read())
 
+    with open(filename, 'w') as f:
+        f.write(template.render(**data))
+
+
+if __name__ == "__main__":
+    render("scripts/communication.zeek")
